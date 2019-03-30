4f92c9b842445d039e7d08c4907a27da04fba287
00400815@uca.edu.sv <16137238>
2019-03-29 21:52:04 -0600

    pre final

--- app/src/main/java/com/kmina/myapplication/Main2Activity.java
+++ app/src/main/java/com/kmina/myapplication/Main2Activity.java
@@ -1,4 +1,96 @@
 package com.kmina.myapplication;
 
-public class Main2Activity {
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView tv_user,tv_email,tv_1,tv_2,tv_3,tv_4,tv_5,tv_6,tv_7,tv_8,tv_9;
+    //String user, pass, email, gender, string_share;
+    Button mButton_Share;
+    String json="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tv_1=findViewById(R.id.tv_prod1);
+        tv_2=findViewById(R.id.tv_prod2);
+        tv_3=findViewById(R.id.tv_prod3);
+        tv_4=findViewById(R.id.tv_prod4);
+        tv_5=findViewById(R.id.tv_prod5);
+        tv_6=findViewById(R.id.tv_prod6);
+        tv_7=findViewById(R.id.tv_prod7);
+        tv_8=findViewById(R.id.tv_prod8);
+        tv_9=findViewById(R.id.tv_prod9);
+
+        tv_user=findViewById(R.id.et_usuario);
+        tv_email=findViewById(R.id.et_correo);
+
+        mButton_Share = findViewById(R.id.bt_send);
+
+
+        Intent mIntent = this.getIntent();
+
+
+        Gson gson = new Gson();  //Creamos un objeto Gson
+
+        if(mIntent != null){
+            //Recuperamos la cadena json
+            json = mIntent.getStringExtra("json");
+
+            //Creamos un nuevo Usuario a partir de json
+            Producto producto = gson.fromJson(json, Producto.class);
+            tv_1.setText(producto.getProducto1());
+            tv_2.setText(producto.getProducto2());
+            tv_3.setText(producto.getProducto3());
+            tv_4.setText(producto.getProducto4());
+            tv_5.setText(producto.getProducto5());
+            tv_6.setText(producto.getProducto6());
+            tv_7.setText(producto.getProducto7());
+            tv_8.setText(producto.getProducto8());
+            tv_9.setText(producto.getProducto9());
+            tv_1.setText(producto.getProducto1());
+            tv_1.setText(producto.getProducto1());
+
+            tv_user.setText(producto.getUsuario());
+            tv_email.setText(producto.getEmail());
+
+        }
+
+        final String mJson_share = json;
+
+        mButton_Share.setOnClickListener(v -> {
+            Intent mIntent_Share = new Intent();
+
+            mIntent_Share.setAction(Intent.ACTION_SEND);
+            mIntent_Share.setType("text/plain");
+
+            mIntent_Share.putExtra(Intent.EXTRA_TEXT,mJson_share);
+
+            if (mIntent_Share.resolveActivity(getPackageManager()) != null) {
+                startActivity(mIntent_Share);
+            }
+
+
+        });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+
+
+
+
+    }
 }
+
+/*tipos de mime type
+ * recibir texto con intent*/
--- app/src/main/java/com/kmina/myapplication/MainActivity.java
+++ app/src/main/java/com/kmina/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.kmina.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.google.gson.Gson;
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout ll_1, ll_2, ll_4, ll_3, ll_5, ll_6, ll_7, ll_8, ll_9;
     TextView tv_1,tv_2,tv_3,tv_4,tv_5,tv_6,tv_7,tv_8,tv_9;
     int prod1, prod2, prod3, prod4 ,prod5, prod6, prod7, prod8, prod9;
+    EditText mEdit_user, mEdit_email;
     Button bt;
 
     @Override
@@ -39,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         tv_7=findViewById(R.id.tv_prod7);
         tv_8=findViewById(R.id.tv_prod8);
         tv_9=findViewById(R.id.tv_prod9);
+
+        mEdit_user=findViewById(R.id.et_usuario);
+        mEdit_email=findViewById(R.id.et_correo);
 
         bt=findViewById(R.id.bt_send);
 
@@ -102,11 +108,11 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        //Creamos un nuevo objeto Usuario y le asignamos las propiedades nombre y edad
-        Producto producto = new Producto();
-        //Creamos un objeto Gson
-        Gson gson = new Gson();
-        //String json = gson.toJson(usuario);
+
+        Producto producto = new Producto(); //Creamos un nuevo objeto Usuario y le asignamos las propiedades nombre y edad
+
+        Gson gson = new Gson();//Creamos un objeto Gson
+
 
         bt.setOnClickListener(v -> {
             producto.setProducto1(prod1+"");
@@ -118,20 +124,20 @@ public class MainActivity extends AppCompatActivity {
             producto.setProducto7(prod7+"");
             producto.setProducto8(prod8+"");
             producto.setProducto9(prod9+"");
+            producto.setUsuario(mEdit_user.getText().toString());
+            producto.setEmail(mEdit_email.getText().toString());
+            //usuario.setmUser(mEditText1.getText().toString());
 
-            //Log.d("qwerty","rrrrr: "+usuario.getmUser());
+            Log.d("qwerty","rrrrr: "+producto.getUsuario());
 
 
 
             //Convertimos el objeto usuario a json
-            String json = gson.toJson(usuario);
-            // Log.d("qwerty",json);
+            String json = gson.toJson(producto);
+             Log.d("qwerty",json);
 
             Intent mIntent = new Intent(MainActivity.this, Main2Activity.class);
             mIntent.putExtra("json",json);
-            //mIntent.putExtra(AppConstants.PASS_KEY,mEditText2.getText().toString());
-            //mIntent.putExtra(AppConstants.EMAIL_KEY,mEditText3.getText().toString());
-            //mIntent.putExtra(AppConstants.GENDER_KEY,mEditText4.getText().toString());
             startActivity(mIntent);
         });
 
